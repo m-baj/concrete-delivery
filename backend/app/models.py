b75a5e8d5313c3f8aeee843d37c9f99fc483a31c
Maksymilian Baj <bajmaks@gmail.com>
2024-12-11 01:14:53 +0100

    add docker caching

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,7 +1,6 @@
 # This file contains the model classes for the database tables.
 from sqlmodel import SQLModel, Field
 
-
 class Status(SQLModel, table=True):
     StatusID: int = Field(primary_key=True)
     Name: str = Field(max_length=255, unique=True, index=True)
