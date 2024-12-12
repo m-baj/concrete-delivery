cd5116291d5666f0992be4cd66316a6eef56b5d0
Maksymilian Baj <bajmaks@gmail.com>
2024-12-12 01:31:48 +0100

    add user models for creating user

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,6 +1,22 @@
 # This file contains the model classes for the database tables.
+import uuid
+
 from sqlmodel import SQLModel, Field
+from pydantic import EmailStr
 
 class Status(SQLModel, table=True):
-    StatusID: int = Field(primary_key=True)
+    Status: int = Field(primary_key=True)
     Name: str = Field(max_length=255, unique=True, index=True)
+
+class UserBase(SQLModel):
+    name: str = Field(max_length=40)
+    surname: str = Field(max_length=40)
+    phone_number: str
+    email_address: EmailStr | None = Field(default=None, max_length=255)
+
+class UserRegister(UserBase):
+    password: str = Field(min_length=8, max_length=40)
+
+class User(UserBase, table=True):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    hashed_password: str
