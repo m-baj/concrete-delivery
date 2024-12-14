9d460a4e442543657961ca7ab079e289f5bf3f23
Maksymilian Baj <bajmaks@gmail.com>
2024-12-14 20:43:12 +0100

    added creating JWT token function

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,9 +1,22 @@
+import jwt
 from passlib.context import CryptContext
+from datetime import timedelta, datetime
+from typing import Any
+
+from app.core.config import settings
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+ALGORITHM = "HS256"
 
 def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
 
 def verify_password(plain_password: str, hashed_password) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(subject: str | Any, expires_delta: timedelta) -> str:
+    expire = datetime.now() + expires_delta
+    to_encode = {"exp": expire, "sub": str(subject)}
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
