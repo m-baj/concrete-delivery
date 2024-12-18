a0c0082881f2ef853f6b42f12460ae94adc9c1ba
Adam Kraś <adam.kras.stud@pw.edu.pl>
2024-12-18 14:11:48 +0100

    Updated models_neo4j.py to use more typical neontology

--- backend/app/models_neo4j.py
+++ backend/app/models_neo4j.py
@@ -1,26 +1,34 @@
-from neontology import Node, Property, Relationship
-from neontology import Point
+from neontology import BaseNode, BaseRelationship
+from typing import ClassVar, List
 
-class Location(Node):
-    locationID = Property(int, unique=True)
-    address = Property(str, required=False)
-    coordinates = Property(Point)
-    orderID = Property(int, required=False)
+class Location(BaseNode):
+    __primarylabel__: ClassVar[str] = "Location"
+    __primaryproperty__: ClassVar[int] = "locationID"
 
-class Courier(Node):
-    courierID = Property(int, unique=True)
-    name = Property(str)
-    locationID = Property(int)
+    locationID: int
+    address: str
+    coordinates: List[float]
+    orderID: int
 
-class IsAt(Relationship):
-    start_node = Courier
-    end_node = Location
+class Courier(BaseNode):
+    __primarylabel__: ClassVar[str] = "Courier"
+    __primaryproperty__: ClassVar[int] = "courierID"
 
-class DeliversTo(Relationship):
-    start_node = Courier
-    end_node = Location
-    pass
+    courierID: int
+    name: str
+    locationID: int
 
-class Then(Relationship):
-    start_node = Location
-    end_node = Location
+class IsAt(BaseRelationship):
+    __relationshiptype__: ClassVar[str] = "IS_AT"
+    source: Courier
+    target: Location
+
+class DeliversTo(BaseRelationship):
+    __relationshiptype__: ClassVar[str] = "DELIVERS_TO"
+    source: Courier
+    target: Location
+
+class Then(BaseRelationship):
+    __relationshiptype__: ClassVar[str] = "THEN"
+    source: Location
+    target: Location
