ecaf32cea7b434e1cece0966a565fcb5e58958c8
Krzysztof Król <krzysztof.krol.stud@pw.edu.pl>
2024-12-18 17:39:33 +0100

    Remove redundant CORS origins configuration in main.py (merging issues fixed)

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,17 +6,6 @@ from app.api.main import router
 
 
 app = FastAPI(title=settings.PROJECT_NAME)
-<<<<<<< Updated upstream
-
-origins = [
-    "http://localhost",
-    "http://localhost:3000",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-=======
 
 origins = [
     "http://localhost:3000",
@@ -25,7 +14,6 @@ origins = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,  # Zezwala na dowolne źródła (używaj tylko na testach)
->>>>>>> Stashed changes
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
