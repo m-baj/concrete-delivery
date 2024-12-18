a5c8644727cc48362bb4790d4af927c8e9f31d96
Adam Kraś <adam.kras.stud@pw.edu.pl>
2024-12-18 11:35:12 +0100

    Moved neo4j configs to general config.py

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,8 +1,8 @@
 import secrets
-
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic_core import MultiHostUrl
 from pydantic import PostgresDsn, computed_field
+from neontology import Database
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
@@ -18,6 +18,13 @@ class Settings(BaseSettings):
     POSTGRES_PASSWORD: str
     POSTGRES_DB: str
 
+    NEO4J_URL: str = "bolt://localhost:7687"
+    NEO4J_AUTH: str = "neo4j/password"
+
+    SUPERUSER_PHONE_NUMBER: str = "000000000"
+    SUPERUSER_EMAIL_ADDRESS: str = "superuser@test.com"
+    SUPERUSER_PASSWORD: str
+
     @computed_field
     @property
     def DATABASE_URL(self) -> PostgresDsn:
@@ -27,11 +34,23 @@ class Settings(BaseSettings):
             password=self.POSTGRES_PASSWORD,
             host=self.POSTGRES_SERVER,
             port=self.POSTGRES_PORT,
-            path=self.POSTGRES_DB,
+            path=f"/{self.POSTGRES_DB}",
         )
 
-    SUPERUSER_PHONE_NUMBER: str = "000000000"
-    SUPERUSER_EMAIL_ADDRESS: str = "superuser@test.com"
-    SUPERUSER_PASSWORD: str
+    @computed_field
+    @property
+    def NEO4J_USER(self) -> str:
+        return self.NEO4J_AUTH.split("/")[0]
 
+    @computed_field
+    @property
+    def NEO4J_PASSWORD(self) -> str:
+        return self.NEO4J_AUTH.split("/")[1]
+
+    def get_database(self) -> Database:
+        return Database(self.NEO4J_URL, self.NEO4J_USER, self.NEO4J_PASSWORD)
+
+
+# Instantiate settings and initialize the database
 settings = Settings()
+database = settings.get_database()
