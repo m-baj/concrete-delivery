9fabe181fc2514b3094cffe91062ff951370c90f
Adam Kraś <adam.kras.stud@pw.edu.pl>
2024-12-18 22:26:24 +0100

    Added models and response models for neo4j

---
+++ backend/app/api_models_neo4j.py
@@ -0,0 +1,32 @@
+from typing import List, Optional
+from pydantic import BaseModel
+
+
+#Response model for location
+class LocationAPI(BaseModel):
+    locationID: int
+    address: str
+    coordinates: List[float]
+    next_location: Optional['LocationAPI'] = None
+
+#Response model for list of locations
+class LocationsAPI(BaseModel):
+    locations: List[LocationAPI]
+
+#Response model for courier
+class CourierAPI(BaseModel):
+    courierID: int
+    name: str
+    current_location: LocationAPI
+    deliveries: List[LocationAPI]
+
+#Request model for adding locations to a courier
+class AddLocationsRequest(BaseModel):
+    locations: List[LocationAPI]
+    
+#Request model for creating a courier
+class CreateCourierRequest(BaseModel):
+    courierID: int
+    name: str
+    current_location: LocationAPI
+    deliveries: List[LocationAPI]
--- backend/app/models_neo4j.py
+++ backend/app/models_neo4j.py
@@ -3,21 +3,21 @@ from neomodel import (
     StringProperty,
     RelationshipTo,
     RelationshipFrom,
-    UniqueIdProperty,
+    IntegerProperty,
 )
 from neomodel.contrib.spatial_properties import NeomodelPoint
 
 class Courier(StructuredNode):
-    courierID = UniqueIdProperty()
+    courierID = IntegerProperty(unique_index=True)
     name = StringProperty()
 
     is_at = RelationshipTo('Location', 'IS_AT')
     delivers_to = RelationshipTo('Location', 'DELIVERS_TO')
 
 class Location(StructuredNode):
-    locationID = UniqueIdProperty()
+    locationID = IntegerProperty(unique_index=True)
     address = StringProperty()
-    coordinates = NeomodelPoint()
+    coordinates = NeomodelPoint(x=0.0, y=0.0, crs='cartesian')
 
     is_visited_by = RelationshipFrom('Courier', 'IS_AT')
     delivered_by = RelationshipFrom('Courier', 'DELIVERS_TO')
