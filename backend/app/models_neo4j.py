1175806a9bb634cc26ac97554ad6420c752a1967
Adam Kraś <adam.kras.stud@pw.edu.pl>
2024-12-18 11:57:50 +0100

    Refactored neo4j models

--- backend/app/models_neo4j.py
+++ backend/app/models_neo4j.py
@@ -2,28 +2,25 @@ from neontology import Node, Property, Relationship
 from neontology import Point
 
 class Location(Node):
-    locationID = Property(int, unique=True)  # Unique identifier for each location
-    address = Property(str, required=False)  # Optional address property
-    coordinates = Property(Point)  # Geospatial coordinates (Neo4j Point data type)
-    orderID = Property(int, required=False)  # Optional order ID
+    locationID = Property(int, unique=True)
+    address = Property(str, required=False)
+    coordinates = Property(Point)
+    orderID = Property(int, required=False)
 
 class Courier(Node):
-    courierID = Property(int, unique=True)  # Unique identifier for the courier
-    name = Property(str)  # Courier's name
-    locationID = Property(int)  # Foreign key to Location (the current location of the courier)
+    courierID = Property(int, unique=True)
+    name = Property(str)
+    locationID = Property(int)
 
-# Define the IS_AT relationship between Courier and Location
 class IsAt(Relationship):
     start_node = Courier
     end_node = Location
 
-# Define the DELIVERS_TO relationship between Courier and Location
 class DeliversTo(Relationship):
     start_node = Courier
     end_node = Location
     pass
 
-# Define the THEN relationship between Location nodes
 class Then(Relationship):
     start_node = Location
     end_node = Location
