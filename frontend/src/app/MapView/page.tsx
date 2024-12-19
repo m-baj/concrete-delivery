ce9613a82c8ecc642a937c33c9dc36488cf90973
Adam <adam.filewicz.stud@pw.edu.pl>
2024-12-19 13:40:17 +0100

    added map_view

--- frontend/src/app/MapView/page.tsx
+++ frontend/src/app/MapView/page.tsx
@@ -1,15 +1,54 @@
 import React from "react";
 import MapViewBox from "@/components/mapViewBox";
 import MapView from "@/components/mapView";
-import { Flex, AspectRatio, Box } from "@chakra-ui/react";
+import {Flex, AspectRatio, Box, VStack, HStack, Spinner, Text} from "@chakra-ui/react";
+
 
 const MapViewPage = () => {
   return (
-    <AspectRatio maxW="800px" ratio={16 / 9}>
-      <Box>
-        <MapView />
-      </Box>
-    </AspectRatio >
+
+          // <VStack spacing={4} align="stretch" w="100%" maxW="1000px" mx="auto">
+          //   <AspectRatio maxW="1000px" ratio={16 / 9}>
+          //       <MapView />
+          //   </AspectRatio >
+          // </VStack>
+      <Flex flex="1" direction="column">
+                <Box
+            height="80%"
+            backgroundColor={"gray"}
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            boxShadow="0 4px 6px rgba(0, 0, 0, 0.1)"
+            borderRadius="md"
+            marginBottom={10}
+        >
+          <iframe
+              src="http://localhost:8000/map" // Adres API zwracającego mapę
+              style={{width: "100%", height: "100%", border: "none"}} // Ustawienia iframe
+              title="Embedded Map"
+          />
+        </Box>
+        <Box
+            backgroundColor={"white"}
+            height={"20%"}
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="left"
+            boxShadow="0 4px 6px rgba(0, 0, 0, 0.1)"
+          borderRadius="md"
+        >
+          <Text fontSize={"xl"} marginLeft={8}>
+            NEXT STOP:
+          </Text>
+
+            <Text fontSize="xx-large" fontWeight="bold" marginLeft={8}>
+              {"No Data"}
+            </Text>
+        </Box>
+      </Flex>
+
   );
 };
 
--- frontend/src/components/mapViewBox.tsx
+++ frontend/src/components/mapViewBox.tsx
@@ -1,12 +1,25 @@
 import MapView from "./mapView";
 import React from "react";
-import { Container, Box, AspectRatio } from "@chakra-ui/react";
+import {Container, Box, AspectRatio, Spinner} from "@chakra-ui/react";
 
 const MapViewBox = () => {
     return (
-        <AspectRatio maxW='800px' ratio={16 / 9}>
-            <MapView />
-        </AspectRatio>
+            <Box
+                height="80%"
+                backgroundColor={"gray.100"}
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                boxShadow="0 4px 6px rgba(0, 0, 0, 0.1)"
+                borderRadius="md"
+                marginBottom={3}
+            >
+              <iframe
+                  src="http://localhost:8000/map" // Adres API zwracającego mapę
+                  style={{width: "100%", height: "100%", border: "none"}} // Ustawienia iframe
+                  title="Embedded Map"
+              />
+            </Box>
     );
 }
 
