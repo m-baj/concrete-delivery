aacdf8d5a42880dbd4a5c8e4ff3093800d190e79
mbaj <01178508@pw.edu.pl>
2025-01-05 20:21:17 +0100

    Add geocoding utility functions for address and coordinates retrieval

---
+++ backend/app/utils/geocoding.py
@@ -0,0 +1,13 @@
+from geopy.geocoders import Nominatim
+from pprint import pprint
+from typing import Tuple
+
+geolocator = Nominatim(user_agent="concrete_delivery")
+
+def get_coordinates(address: str) -> Tuple[float, float]:
+    location = geolocator.geocode(address)
+    return location.latitude, location.longitude
+
+def get_address(coordinates: Tuple[float, float]) -> str:
+    location = geolocator.reverse(coordinates)
+    return location.address
