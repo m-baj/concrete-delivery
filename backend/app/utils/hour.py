075029245eb69f3540b82c33fb8bb9ce67f7e3e0
mbaj <01178508@pw.edu.pl>
2025-01-06 15:54:19 +0100

    Refactor hour conversion function to use datetime for improved parsing and error handling

--- backend/app/utils/hour.py
+++ backend/app/utils/hour.py
@@ -1,13 +1,9 @@
+from datetime import datetime
+
 def hour_from_str_to_seconds(hour: str) -> int:
-    if len(hour) == 5:
-        time_in_seconds = (
-            int(hour[0]) * 36000
-            + int(hour[1]) * 3600
-            + int(hour[3]) * 600
-            + int(hour[4]) * 60
-        )
-    elif len(hour) == 4:
-        time_in_seconds = int(hour[0]) * 3600 + int(hour[2]) * 600 + int(hour[3]) * 60
-    else:
-        raise ValueError("Invalid hour format")
-    return time_in_seconds
+    try:
+        time_obj = datetime.strptime(hour, "%H.%M").time()
+        time_in_seconds = time_obj.hour * 3600 + time_obj.minute * 60
+        return time_in_seconds
+    except ValueError:
+        raise ValueError("Invalid hour format. Expected format 'HH.MM' or 'H.MM'")
