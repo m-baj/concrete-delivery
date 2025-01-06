12de019f90f0dca97093634316a6b818de11aeda
mbaj <01178508@pw.edu.pl>
2025-01-06 15:37:21 +0100

    Implement route updating in Neo4jUpdater to write locations to couriers

--- backend/app/utils/neo4j_updater.py
+++ backend/app/utils/neo4j_updater.py
@@ -2,6 +2,8 @@ from dataclasses import dataclass
 from typing import Dict
 
 from app.utils.vroom.models import OptimizationResult
+import app.crud_neo4j as crud_neo4j
+
 
 @dataclass
 class Neo4jUpdater:
@@ -9,4 +11,7 @@ class Neo4jUpdater:
     vehicle_id_to_courier_id: Dict[int, str]
 
     def update_routes(self):
-        pass
+        for route in self.optimization_result.routes:
+            courier_id = self.vehicle_id_to_courier_id[route.vehicle]
+            locations = [step.location for step in route.steps]
+            crud_neo4j.write_locations_to_courier(courier_id, locations)
