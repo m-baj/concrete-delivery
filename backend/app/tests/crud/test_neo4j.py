481e8cf5b418cb5adcbeae0e69692e1870902f91
Krzysztof Król <krzysztof.krol.stud@pw.edu.pl>
2025-01-11 09:26:33 +0100

    Refactor test_neo4j.py for improved readability and consistency

--- backend/app/tests/crud/test_neo4j.py
+++ backend/app/tests/crud/test_neo4j.py
@@ -2,11 +2,18 @@ import pytest
 import app.crud_neo4j as crud
 from neomodel import db, clear_neo4j_database
 from app.models_neo4j import Courier, Location
-from app.api_models_neo4j import LocationAPI, LocationsAPI, CourierAPI, AddLocationsRequest
+from app.api_models_neo4j import (
+    LocationAPI,
+    LocationsAPI,
+    CourierAPI,
+    AddLocationsRequest,
+)
+
 
 @pytest.fixture(autouse=True)
 def clear_db():
     clear_neo4j_database(db)
+
 
 def test_create_courier():
     courier_id = "1"
@@ -15,6 +22,7 @@ def test_create_courier():
     assert courier.courierID == courier_id
     assert courier.name == name
 
+
 def test_get_courier_by_id():
     courier_id = "1"
     test_courier = crud.create_courier(courier_id=courier_id, name="Test")
@@ -22,21 +30,28 @@ def test_get_courier_by_id():
     assert courier.courierID == courier_id
     assert courier.name == "Test"
 
+
 def test_create_location():
     location_id = "1"
     address = "Test Address"
     coordinates = [1.0, 1.0]
-    location = crud.create_location(location_id=location_id, address=address, coordinates=coordinates)
+    location = crud.create_location(
+        location_id=location_id, address=address, coordinates=coordinates
+    )
     assert location.locationID == location_id
     assert location.address == address
     assert location.coordinates == coordinates
 
+
 def test_get_location_by_id():
     location_id = "1"
-    test_location = crud.create_location(location_id=location_id, address="Test Address", coordinates=[1.0, 1.0])
+    test_location = crud.create_location(
+        location_id=location_id, address="Test Address", coordinates=[1.0, 1.0]
+    )
     location = crud.get_location_by_id(location_id=location_id)
     assert location.locationID == location_id
     assert location.address == "Test Address"
+
 
 def test_connect_delivers_to():
     courier = crud.create_courier("1", "Courier 1")
@@ -44,6 +59,7 @@ def test_connect_delivers_to():
     crud.connect_delivers_to(courier, location)
     assert len(courier.delivers_to.all()) == 1
     assert courier.delivers_to.all()[0].locationID == location.locationID
+
 
 def test_disconnect_all_delivers_to():
     courier = crud.create_courier("1", "Courier 1")
@@ -56,11 +72,13 @@ def test_disconnect_all_delivers_to():
     crud.disconnect_all_delivers_to(courier)
     assert len(courier.delivers_to.all()) == 0
 
+
 def test_connect_next_location():
     location1 = crud.create_location("1", "Address 1", [0.0, 0.0])
     location2 = crud.create_location("2", "Address 2", [1.0, 1.0])
     crud.connect_next_location(location1, location2)
     assert location1.next_location.single().locationID == location2.locationID
+
 
 def test_get_current_location():
     courier = crud.create_courier("1", "Courier 1")
@@ -69,12 +87,14 @@ def test_get_current_location():
     current_location = crud.get_current_location(courier)
     assert current_location.locationID == location.locationID
 
+
 def test_disconnect_current_location():
     courier = crud.create_courier("1", "Courier 1")
     location = crud.create_location("1", "Address 1", [0.0, 0.0])
     crud.connect_current_location(courier, location)
     crud.disconnect_current_location(courier, location)
     assert courier.is_at.all() == []
+
 
 def test_connect_current_location():
     courier = crud.create_courier("1", "Courier 1")
